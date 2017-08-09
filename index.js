499abf03cf5ce0af0136cbbb9d44b184a42924ef
Sebastian Karasiewicz <sebastian.karasiewicz@sofomo.com>
2017-08-09 19:21:47 +0200

    new changes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 let mainWindow
+const { setMainMenu } = require('./main-menu.js')
 
 app.on('ready', () => {
+  // Prevent from show before everything is loaded
   mainWindow = new BrowserWindow({
     show: false
   })
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
+  // Display when all elements are loaded
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
+  setMainMenu()
 })
